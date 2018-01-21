8ef956df1941df43ac441e1fb0cad5eb2df3434a
hugu1026 <hugu1026@gmail.com>
2018-01-21 19:36:51 +0900

    [add]魔法: ファイヤを追加

--- src/main/java/hugu1026/com/github/phantasymagic/magic/Fire.java
+++ src/main/java/hugu1026/com/github/phantasymagic/magic/Fire.java
@@ -1,17 +1,29 @@
 package hugu1026.com.github.phantasymagic.magic;
 
 import hugu1026.com.github.phantasymagic.event.ActivateMagicEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.Event;
+
+import java.util.Collection;
 
 public class Fire extends Magic {
 
-    public Fire(String magicName, Event event) {
-        super(magicName, event);
+    public Fire(String magicName, Event event, Integer slot) {
+        super(magicName, event, slot);
     }
 
     @Override
-    public void ActivatedMagic(ActivateMagicEvent event) {
-        event.getPlayer().sendMessage("fire occurred");
+    public void ActivatedMagic(ActivateMagicEvent event, Location magicLocation) {
+        Collection<Entity> entities = Bukkit.getServer().getWorld("world").getNearbyEntities(magicLocation, 0.25, 5, 0.25);
+        for (Entity entity : entities) {
+            entity.setFireTicks(20 * 5);
+        }
+        Bukkit.getWorld("world").spawnParticle(Particle.LAVA, magicLocation.add(0, 1, 0), 20);
+        Bukkit.getWorld("world").playSound(magicLocation, Sound.ENTITY_GHAST_SHOOT, 1, 0);
     }
 
     @Override
