16b3f9bdc6bc766d10616d780476faea6d5c4163
hugu1026 <hugu1026@gmail.com>
2018-01-22 20:08:56 +0900

    [fix]敵対MOB以外のエンティティに反応していた

--- src/main/java/hugu1026/com/github/phantasymagic/magic/Fire.java
+++ src/main/java/hugu1026/com/github/phantasymagic/magic/Fire.java
@@ -1,10 +1,10 @@
 package hugu1026.com.github.phantasymagic.magic;
 
 import hugu1026.com.github.phantasymagic.event.ActivateMagicEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.Event;
 
@@ -20,7 +20,10 @@ public class Fire extends Magic {
     public void ActivatedMagic(ActivateMagicEvent event, Location magicLocation) {
         Collection<Entity> entities = event.getPlayer().getWorld().getNearbyEntities(magicLocation, 0.25, 5, 0.25);
         for (Entity entity : entities) {
-            entity.setFireTicks(20 * 5);
+            if (entity instanceof Creature) {
+                Creature creature = (Creature) entity;
+                creature.setFireTicks(20 * 5);
+            }
         }
         event.getPlayer().getWorld().spawnParticle(Particle.LAVA, magicLocation.add(0, 1, 0), 20);
         event.getPlayer().getWorld().playSound(magicLocation, Sound.ENTITY_GHAST_SHOOT, 1, 0);
