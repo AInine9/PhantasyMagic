1ee9010d9750c3ce39b627ad1ed052b90c79f4f1
hugu1026 <hugu1026@gmail.com>
2018-02-26 15:17:13 +0900

    [add]エフェクト追加、既に開放した魔法は開放できないように

--- src/main/java/hugu1026/com/github/phantasymagic/listener/MagicBookClicked.java
+++ src/main/java/hugu1026/com/github/phantasymagic/listener/MagicBookClicked.java
@@ -3,6 +3,8 @@ package hugu1026.com.github.phantasymagic.listener;
 import hugu1026.com.github.phantasymagic.event.MagicBookClickedEvent;
 import hugu1026.com.github.phantasystatus.util.PlayerDataUtil;
 import org.bukkit.ChatColor;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,8 +19,15 @@ public class MagicBookClicked implements Listener {
         String magicName = event.getMagicName();
         String magicDisplayName = event.getMagicDisplayName();
 
+        if (PlayerDataUtil.getMagicList(player).contains(magicName)) {
+            player.sendMessage(ChatColor.RED + "その魔法は既に開放されている！");
+            return;
+        }
         PlayerDataUtil.addMagicList(player, magicName);
         player.getInventory().removeItem(magicBook);
         player.sendMessage(ChatColor.DARK_PURPLE + "新たな魔法: " + ChatColor.RED + magicDisplayName + ChatColor.DARK_PURPLE + "が開放された！");
+        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_WITHER_SPAWN, 1, (float) 1.5);
+        player.getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, player.getLocation().add(0, 0.5, 0), 200);
+        player.getWorld().spawnParticle(Particle.SPELL_WITCH, player.getLocation().add(0, 0.5, 0), 100);
     }
 }
